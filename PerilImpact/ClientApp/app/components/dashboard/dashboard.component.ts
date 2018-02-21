c8d9e773bed1d21de0067e23f825080e664ad882
rahul <rahul@localligence.com>
2018-02-21 18:31:25 +0530

    commit

---
+++ PerilImpact/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'pi-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
+})
+export class DashBoard { }
